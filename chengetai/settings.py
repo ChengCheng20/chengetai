69d0f4b3756400fdcad924785f73cbbf0f6042c0
Chengetai Chitungo <chengetai09@yahoo.com>
2020-06-07 20:46:36 +0200

    Allowed host set

--- chengetai/settings.py
+++ chengetai/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 #DEBUG = True
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['chenchitun.herokuapp.com']
 
 
 # Application definition
